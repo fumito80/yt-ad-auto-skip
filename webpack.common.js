7817c62826f8f6d9190f1ce848ca0fbec2167352
fumito80 <fumito80@gmail.com>
2024-04-17 16:23:48 +0900

    format

--- webpack.common.js
+++ webpack.common.js
@@ -2,40 +2,15 @@
 const CopyPlugin = require('copy-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const WebpackNotifierPlugin = require('webpack-notifier');
 
 module.exports = {
   entry: {
     script: './src/script.js',
   },
-  output: {
-    globalObject: 'self',
-    filename: '[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: [
-          {
-            loader: 'ts-loader',
-          },
-        ],
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-    ],
-  },
   resolve: {
-    extensions: ['.ts', '.js', '.json'],
+    extensions: ['.js'],
   },
   target: ['web', 'es2021'],
-  cache: true,
   watchOptions: {
     poll: true,
   },
@@ -43,17 +18,12 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: '*.*', context: 'assets/' },
-        // { from: '*.html', context: 'src/view/' },
-        // { from: '*.png', context: 'src/images/' },
-        // { from: '*.json', context: 'src/_locales/en/', to: '_locales/en' },
-        // { from: '*.json', context: 'src/_locales/ja/', to: '_locales/ja' },
       ],
     }),
     new ESLintPlugin({
-      extensions: ['.ts', '.js'],
+      extensions: ['.js'],
       exclude: 'node_modules',
     }),
     new CleanWebpackPlugin(),
-    // new WebpackNotifierPlugin({ alwaysNotify: true }),
   ],
 };
