3b00c8d573d0f39ec1f39e734efa88e7e9948b58
fumito80 <fumito80@gmail.com>
2024-09-05 20:37:34 +0900

    v0.14.2

--- src/prescript.js
+++ src/prescript.js
@@ -2,26 +2,28 @@ function ytAdMaxDig(el, els = []) {
   if (!el) {
     return [...els, window];
   }
-  return ytAdMaxDig(el.parentElement, [el, ...els]);
+  return ytAdMaxDig(el.parentElement, [...els, el]);
 }
 
-HTMLButtonElement.prototype.addEventListenerOrg ??= HTMLButtonElement.prototype.addEventListener;
-HTMLButtonElement.prototype.addEventListener = function a(type, listener, options) {
-  if (type !== 'click') {
-    return this.addEventListenerOrg(type, listener, options);
-  }
-  const hookListener = (...args) => {
-    const [arg1, ...rest] = args;
-    if (arg1.clientX !== 999999) {
-      return listener(...args);
+if (!HTMLButtonElement.prototype.addEventListenerOrg) {
+  HTMLButtonElement.prototype.addEventListenerOrg = HTMLButtonElement.prototype.addEventListener;
+  HTMLButtonElement.prototype.addEventListener = function a(type, listener, options) {
+    if (type !== 'click') {
+      return this.addEventListenerOrg(type, listener, options);
     }
-    const els = ytAdMaxDig(this);
-    return listener({
-      ...arg1,
-      isTrusted: true,
-      preventDefault: () => true,
-      composedPath: () => els,
-    }, ...rest);
+    const hookListener = (...args) => {
+      const [arg1, ...rest] = args;
+      if (arg1.clientX !== 999999) {
+        return listener(...args);
+      }
+      const els = ytAdMaxDig(this);
+      return listener({
+        ...arg1,
+        isTrusted: true,
+        preventDefault: () => true,
+        composedPath: () => els,
+      }, ...rest);
+    };
+    return this.addEventListenerOrg(type, hookListener, options);
   };
-  return this.addEventListenerOrg(type, hookListener, options);
-};
+}
